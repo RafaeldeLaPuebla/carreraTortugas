8660bbde510895d3d29e9f6be9a5de9b594ab091
RafaeldeLaPuebla <46501857+RafaeldeLaPuebla@users.noreply.github.com>
2019-01-10 23:34:36 +0100

    Add files via upload

---
+++ ejemploGetterySetter.py
@@ -0,0 +1,14 @@
+class ClaseConGetterySetter():
+    def __init__(self):
+        self.__propiedad_privada = None
+    
+    def setPropiedadPrivada (self, valor):
+        self.__propiedad_privada = valor
+    
+    def __str__(self):
+        return "ClaseConGetterySetter con propiedadPrivada -> {}".format(self.__propiedad_privada)
+    
+    
+    
+if __name__== '__main__':
+    c = ClaseConGetterySetter()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import random #Importamos el módulo random para lanzar el dado que nos diga cuánto va a avanzar cada tortuga.
 
 class Circuito(): #Voy a crear el circuito.
     corredores = []
@@ -24,12 +25,24 @@ class Circuito(): #Voy a crear el circuito.
             new_turtle.setpos(self.__startLine, self.__posStartY[i])
             
             self.corredores.append(new_turtle)
-
-
-
+    
+    def competir(self):
+        
+        hayGanador = False #Cuando empiezo no hay ganador. Esto no es un atributo. No se puede invocar. Sólo tiene existencia cuando competir está activo.
+        
+        while not hayGanador:
+            for tortuga in self.corredores:
+                avance = random.randint(1,6)
+                tortuga.forward(avance)
+            
+                if tortuga.position()[0] >= self.__finishLine: #Cojo la posición 0 de la tupla que es la coordenada x de la tortuga. En el momento en que toca con el pecho la cinta, es el ganador y se puede pasar.
+                    hayGanador = True
+                    print("La tortuga de color {} ha ganado".format(tortuga.color()[0])) #Es un Getter. Lleva paréntesis porque es una función. Si quieres que se ejecute, tienes que poner paréntesis.
+                    break #Para parar el bucle y que sólo haya un ganador.
 
 if __name__== '__main__':
     circuito = Circuito(640, 480)
+    circuito.competir() #Invoco la competición.
     
         
         
